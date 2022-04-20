99f23faa531a6dd3904a0dbce15c33ca680d252c
Anjali Panchariya <anjalipanchariya11@gmail.com>
2022-04-20 14:04:30 +0530

    first commit by Anjali

---
+++ main1.py
@@ -0,0 +1,13 @@
+while(True):
+    print("""
+            ================================
+               HOSPITAL MANAGEMENT SYSTEM
+            ================================
+    """)
+    ##creating database connectivity
+    import mysql.connector
+    passwd=str(input("ENTER THE DATABASE PASSWORD;"))
+    
+
+
+    
